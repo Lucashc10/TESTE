d6b1d881778af3218a4eac11ced00a0eda3b03c1
Lucas Henrique Capati <161030534+Lucashc10@users.noreply.github.com>
2024-03-01 10:28:38 -0300

    Add files via upload

---
+++ miguel exterminator.js
@@ -0,0 +1,11 @@
+//fotos do miguel programação foda
+//hello miguel
+document.addEventListener("DOMContentLoaded", () => { 
+ const jao = document.body.querySelector("#jao");
+jao.addEventListener("click", ()=> {
+if (jao.textContent==="Miguel")
+ jao.textContent="Miguel2";
+ else
+ jao.textContent="Miguel";
+});
+});
